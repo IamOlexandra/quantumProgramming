0e0e0bd1b5495655fb7867bc318a7a4fe45fc4a6
IamOlexandra <alex.eve.kravchenko@gmail.com>
2025-06-08 20:15:53 +0300

    Update index.js
    I think, I finished all my sections

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-var currentVisible = 0;
+var currentVisible = 0,
+    intervalId = setInterval(autoSlider, 3000);
 function moveSlider(number) {
     document.getElementsByClassName("reviwes-item")[currentVisible].classList.remove("reviwes-visible");
     document.getElementsByClassName("reviwes-item")[number].classList.add("reviwes-visible");
     currentVisible = number;
+}
+function clickSlider(number) {
+    clearInterval(intervalId);
+    moveSlider(number);
+}
+function autoSlider() {
+    var nextItem = currentVisible + 1;
+    if(nextItem > 2) {
+        nextItem = 0;
+    }
+    moveSlider(nextItem);
 }
