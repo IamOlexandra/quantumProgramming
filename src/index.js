2aaa06b1ecd1dcd8993f52be185f5a96eaa97f71
IamOlexandra <alex.eve.kravchenko@gmail.com>
2025-06-24 09:29:33 +0300

    Add modal and more

--- src/index.js
+++ src/index.js
@@ -16,3 +16,21 @@ function autoSlider() {
     }
     moveSlider(nextItem);
 }
+
+window.addEventListener('load', function() {
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[cow-modal-open]"),
+      closeModalBtn: document.querySelector("[cow-modal-close]"),
+      modal: document.querySelector("[cow-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      /*document.body.classList.toggle("no-scroll");*/
+    }
+  })();
+});
