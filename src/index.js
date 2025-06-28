40d2ab061ebe384916563bd970f0c669dfae270f
Alena Lychko <alenaxxprv@gmail.com>
2025-06-28 13:10:47 -0400

    alena

--- src/index.js
+++ src/index.js
@@ -34,3 +34,20 @@ window.addEventListener('load', function() {
     }
   })();
 });
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-mob-open]'),
+    closeModalBtn: document.querySelector('[data-mob-close]'),
+    modal: document.querySelector('[data-mob]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
+})();
+
